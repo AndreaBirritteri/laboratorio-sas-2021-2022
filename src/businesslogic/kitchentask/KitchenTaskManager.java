e8c1894077684a0b2accfd6e4d0a484df44817b3
Rondinella Raoul <raoul.rondinella@edu.unito.it>
2022-08-31 13:18:15 +0200

    added task manager

---
+++ src/businesslogic/kitchentask/KitchenTaskManager.java
@@ -0,0 +1,46 @@
+package businesslogic.kitchentask;
+
+import businesslogic.shift.Shift;
+
+public class KitchenTaskManager {
+
+    public void addReceiver(KitchenTaskReceiver kitchenTaskReceiver) {
+
+    }
+
+    public void removeReceiver(KitchenTaskReceiver kitchenTaskReceiver) {
+
+    }
+
+    public void notifySheetCreated(KitchenSheet sheet) {
+
+    }
+
+    public void notifyTaskAdded(KitchenSheet sheet, KitchenTask task) {
+
+    }
+
+    public void notifyTaskDeleted(KitchenTask task) {
+
+    }
+
+    public void notifySheetRestored(KitchenSheet sheet) {
+
+    }
+
+    public void notifyTasksRearranged(KitchenSheet sheet) {
+
+    }
+
+    public void notifyTaskAssigned(KitchenSheet sheet, KitchenTask task) {
+
+    }
+
+    public void notifyTaskCompleted(KitchenSheet sheet, KitchenTask task) {
+
+    }
+
+    public void notifyTurnCompletenessChanged(Shift shift, boolean isCompleted) {
+
+    }
+}
